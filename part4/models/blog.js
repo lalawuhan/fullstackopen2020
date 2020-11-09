5668381db22e5dff408d4be881298c46452ecf95
Rethabile Segoe <precious.segoe@gmail.com>
2020-11-09 22:36:26 +0100

    fix mongo imports

--- part4/models/blog.js
+++ part4/models/blog.js
@@ -1,20 +1,19 @@
-const mongoose = require("mongoose");
-const url = process.env.MONGODB_URI
+const mongoose = require('mongoose')
 
-  const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-  })
+const blogSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
 
 
-  blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
+blogSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-module.exports = mongoose.model("Blog", blogSchema);
+module.exports = mongoose.model('Blog', blogSchema)
