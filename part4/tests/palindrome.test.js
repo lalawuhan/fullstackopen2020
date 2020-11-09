40f67cfac7549f3ff18abb6779f4a4de56ef8b45
Rethabile Segoe <precious.segoe@gmail.com>
2020-11-09 22:35:32 +0100

    uncomment practice code

--- part4/tests/palindrome.test.js
+++ part4/tests/palindrome.test.js
@@ -1,15 +1,19 @@
-// const average = require('../utils/list_helper').
+const palindrome = require('../utils/for_testing').palindrome
 
-//   describe('average', () => {
-//     test('of one value is the value itself', () => {
-//       expect(average([1])).toBe(1)
-//     })
+test('palindrome of a', () => {
+  const result = palindrome('a')
 
-//     test('of many is calculated right', () => {
-//       expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5)
-//     })
+  expect(result).toBe('a')
+})
 
-//     test('of empty array is zero', () => {
-//       expect(average([])).toBe(0)
-//     })
-//   })
+test('palindrome of react', () => {
+  const result = palindrome('react')
+
+  expect(result).toBe('tcaer')
+})
+
+test('palindrome of releveler', () => {
+  const result = palindrome('releveler')
+
+  expect(result).toBe('releveler')
+})
