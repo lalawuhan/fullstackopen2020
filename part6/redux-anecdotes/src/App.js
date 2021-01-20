be9af2d97d7a6a9e1d195f824301dd6f4a2777fd
Rethabile Segoe <precious.segoe@gmail.com>
2021-01-20 21:06:02 +0100

    add ex6.19

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,10 +1,9 @@
 import React, {useEffect} from 'react'
 import { useDispatch } from 'react-redux'
+import ConnectedAnecdoteList from './components/AnecdoteList'
 import AnecdoteService from './services/anecdotes'
 import { initializeAnecdotes } from './reducers/anecdoteReducer'
-
 import AnecdoteForm from './components/AnecdoteForm'
-import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 
 const App = () => {
@@ -20,7 +19,7 @@ const App = () => {
     <div>
       <Notification />
       <AnecdoteForm />
-      <AnecdoteList />
+      <ConnectedAnecdoteList/>
     </div>
   )
 }
--- part6/redux-anecdotes/src/components/AnecdoteList.js
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from 'react'
+import { connect } from 'react-redux'
 import { addVote } from '../reducers/anecdoteReducer'
-import { createNotification } from '../reducers/notificationReducer'
+import { createNotification, hideNotification} from '../reducers/notificationReducer'
 
 const Anecdote = ({ anecdote, handleClick }) => {
   return (
@@ -17,14 +17,17 @@ const Anecdote = ({ anecdote, handleClick }) => {
   )
 }
 
-const AnecdoteList = () => {
-  const dispatch = useDispatch()
-  const anecdotes = useSelector(state => state.anecdotes) // component can access notes in the store with this hook
-
+const AnecdoteList = (props) => {
+  console.log('props',props)
+  const anecdotes = props.anecdotes
   const vote= (id, anecdote)=> {
-    dispatch(addVote(id))
-    dispatch(createNotification(`You voted for: ${anecdote.content}`))
+    props.addVote(id)
+    props.createNotification(`You voted for: ${anecdote.content}`)
+    setTimeout(() => {
+      props.hideNotification()
+  }, 5000)
   }
+
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -39,4 +42,20 @@ const AnecdoteList = () => {
   )
 }
 
-export default AnecdoteList
+const mapStateToProps = state => {
+  return {
+    anecdotes:state.anecdotes
+  }
+}
+const mapDispatchToProps = {
+  addVote,
+  createNotification,
+  hideNotification,
+}
+
+const ConnectedAnecdoteList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AnecdoteList)
+
+export default ConnectedAnecdoteList
